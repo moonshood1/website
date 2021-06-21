14eb548310ba0722738cf095d8011541453140e2
Louis Roger Guirika <louisrogerguirika@gmail.com>
2021-06-21 11:10:54 +0000

    21/06/2021 v1

---
+++ app.js
@@ -0,0 +1,13 @@
+/*barba.init({
+  transitions: [
+    {
+      name: "default-transition",
+      leave() {
+        // create your stunning leave animation here
+      },
+      enter() {
+        // create your amazing enter animation here
+      },
+    },
+  ],
+});*/
